413a425593661c91bd8338b5f784d383919b101d
siddik-rahman <siddik8976@gmail.com>
2026-06-22 22:19:18 +0600

    Configure Github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/fylo-data-storage-component-master/',
   plugins: [
     tailwindcss(),
   ],
-
-
-
 
 })
